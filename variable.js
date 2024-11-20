6168a6a43032318416fb93e9ac9afda569fe6d01
Maryam Toubali <toubali.circet@gmail.com>
2024-11-20 10:50:13 +0100

    Version 1

---
+++ variable.js
@@ -0,0 +1,77 @@
+//Ex:1
+let name = "Maryam"
+console.log(name)
+    //Ex:2
+let age = 26
+let height = 160
+console.log(age + height)
+    //Ex:3
+let isStudent = true
+console.log(typeof(isStudent))
+    //Ex:4
+let greeting = "hello"
+console.log(greeting + " there")
+    //Ex:5
+const PI = 3.14159
+PI = 3.14
+console.log(PI) //ERROR! 
+    //Ex:6
+let firstName = "Maryam "
+let lastName = "TOUBALI"
+let fullName = firstName + lastName
+console.log(fullName) //Maryam TOUBALI 
+    //Ex:7
+let score = 10
+let doubleScore = 2 * score
+console.log(doubleScore)
+    //Ex:8
+let x
+console.log(typeof(x))
+    //Ex:9
+let colors = ["red", "blue", "pink"]
+console.log(colors[1])
+    //Ex:10
+let person = {
+    name: "Maryam",
+    age: 26,
+    isEmployed: true
+}
+console.log(person.age)
+    //Ex:11
+let num = "100"
+num = Number(num)
+console.log(num)
+console.log(typeof num)
+    //Ex:12
+let isComplete = null
+console.log(isComplete)
+console.log(typeof isComplete)
+    //Ex:13
+let total = 10 + 5 + "10"
+console.log(total) // somme(10+5 concatené avec 10)
+    //Ex:14
+let bigNumber = 2 n ** 53 n
+console.log(bigNumber)
+console.log(typeof bigNumber)
+    //Ex:15
+let temp = 50 / 2
+console.log(temp)
+console.log(typeof temp)
+    //Ex:16
+let numbers = [1, 2, 3]
+numbers.push(4)
+console.log(numbers)
+    //Ex:17
+let book = {
+        title: "text",
+        author: 23,
+        page: ,
+        ..
+
+        //Ex:18
+
+        //Ex:19
+        let boolString = Boolean("false")
+        console.log(boolString) //dans Boolean("false") y a la chaine de 
+        caractère false donc il est remplit donc true c 'est a dire il y a
+        quelque chose dans boolString pruisque en parle de la fct Boolean()
