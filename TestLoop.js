e92a23f685cb255781eeb686a110417a6f19da39
Maryam Toubali <toubali.circet@gmail.com>
2024-11-21 16:15:21 +0100

    commit

--- TestLoop.js
+++ TestLoop.js
@@ -1,9 +1,9 @@
 //1
-for (let i = 1; i <= 30; i++) {
+for (i = 1; i <= 30; i++) {
     if (i % 2 !== 0) { console.log(i) }
 };
 //2
-let tab = ["banana", "fraise", "poivre"]
+tab = ["banana", "fraise", "poivre"]
 for (i = 0; i < tab.length; i++) {
     console.log(tab[i].toUpperCase());
 }
